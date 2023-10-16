db9758ab195b138d11ee6997d1c410b1f218e6a5
Martins Ngene <martinsngene9@gmail.com>
2023-10-16 09:09:22 +0100

    update: Restructure project, change backend stack

--- server/server.py
+++
@@ -1,3 +0,0 @@
-from nylas import APIClient
-from nylas.client.restful_models import Webhook
-from nylas.services.tunnel import open_webhook_tunnel
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import Layout from "@/components/layout";
 import ModalBody from "@/components/page_builders/modal_body";
 
 export default function Home() {
+  const [addId, setAddId] = useState<any>();
+
   // State management for forms
   const [displayModal, setDisplayModal] = useState({
     open: false,
@@ -21,6 +23,31 @@ export default function Home() {
       form: formType,
     });
   };
+
+  let fireBaseArr = [
+    { name: "John" },
+    { name: "Peter" },
+    { name: "Jane" },
+    { name: "Paul" },
+  ];
+
+  // Step 1
+  let _id = ["001", "002", "003", "004"];
+
+  let person = { name: "John Doe", place: "Lagos", age: "56" };
+  // Create a new object with a spread operator and add the id value
+  let newPerson = { id: _id, ...person };
+  // console.log(person)
+  // console.log(newPerson)
+
+  const newArray = fireBaseArr.map((item: any) => {
+    return {
+      ...item,
+      _id: "001", //Add your id
+    };
+  });
+
+  console.log(newArray);
 
   return (
     <Layout>
